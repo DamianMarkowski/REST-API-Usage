7d1f200316119b2dca9a2819df33f9d3347b0675
Damian Markowski <contact@damianmarkowski.pl>
2017-06-25 10:31:05 +0200

    project created

---
+++ REST API Usage/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  REST API Usage
+//
+//  Created by Damian Markowski on 25.06.2017.
+//  Copyright © 2017 Damian Markowski. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ REST API Usage/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  REST API Usage
+//
+//  Created by Damian Markowski on 25.06.2017.
+//  Copyright © 2017 Damian Markowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
